76a72d9f95f50124976a7070018239824a1dde96
Roli Wilhelm <rwilhelm@mail.ubc.ca>
2014-02-22 22:45:52 -0800

    Added sample files

---
+++ Makefile.R
@@ -0,0 +1,12 @@
+source("Block01 - Extracting_Relevant_Data.R")
+
+source("Block02 - Separating Projects and Spot Checking Peak Identifications.R")
+
+source("Block03 - Inputting Confirm Peaks and Double Checking Peak Assignment.R")
+
+source("Block04 - Inputting File Checked for Duplicates and Making Standard Curve.R")
+
+source("Block05 - Quantitation, Normalization, Calculating nmol 13C per g dry wt soil etc.R")
+
+source("Block06 - Assign Taxonomy, Calculate F_to_B and Explore Data.R")
+
